1d7fd0b5735129f36cb3f3c5caa95f77d918302f
Olivier Cailloux <olivier.cailloux@dauphine.fr>
2019-02-12 16:10:55 +0100

    Added constructor

--- src/main/java/io/github/oliviercailloux/y2018/minimax/PartialPreference.java
+++ src/main/java/io/github/oliviercailloux/y2018/minimax/PartialPreference.java
@@ -3,6 +3,7 @@ package io.github.oliviercailloux.y2018.minimax;
 import com.google.common.base.Objects;
 import com.google.common.base.Preconditions;
 import com.google.common.graph.Graph;
+import com.google.common.graph.GraphBuilder;
 import com.google.common.graph.Graphs;
 import com.google.common.graph.MutableGraph;
 
@@ -13,37 +14,47 @@ public class PartialPreference {
 
 	private Voter voter;
 	private MutableGraph<Alternative> pref;
-	
+
+	public PartialPreference(Voter voter) {
+		this(voter, GraphBuilder.directed().build());
+	}
+
 	public PartialPreference(Voter voter, Graph<Alternative> pref) {
 		this.voter = voter;
 		Preconditions.checkNotNull(pref);
-		if(Graphs.hasCycle(pref)) {
+		if (Graphs.hasCycle(pref)) {
 			throw new IllegalArgumentException();
 		}
-		this.pref= Graphs.copyOf(pref);
+		this.pref = Graphs.copyOf(pref);
 	}
 
-	public MutableGraph<Alternative> asGraph(){
+	public MutableGraph<Alternative> asGraph() {
 		return this.pref;
 	}
-	
+
 	public int hashCode() {
-		return Objects.hashCode(this.voter,this.pref);
+		return Objects.hashCode(this.voter, this.pref);
 	}
 
 	public boolean equals(Object o) {
-		if(o == this) return true;
-		if(o == null) return false;
-		if(!(o instanceof PartialPreference)) return false;
-		PartialPreference pp = (PartialPreference)o;
-		if(this.voter!=pp.voter || !this.pref.equals(pp.pref)) 
+		if (o == this) {
+			return true;
+		}
+		if (o == null) {
 			return false;
+		}
+		if (!(o instanceof PartialPreference)) {
+			return false;
+		}
+		PartialPreference pp = (PartialPreference) o;
+		if (this.voter != pp.voter || !this.pref.equals(pp.pref)) {
+			return false;
+		}
 		return true;
 	}
 
 	public String toString() {
-		return this.voter + " : " +this.pref.toString();
+		return this.voter + " : " + this.pref.toString();
 	}
-	
-	
+
 }
