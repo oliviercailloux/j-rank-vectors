bbbbc5d1501d0c6d38145e7cf89a8ef72dddd024
Olivier Cailloux <olivier.cailloux@gmail.com>
2019-02-26 02:10:08 +0100

    Rm outdated comments

--- src/main/java/io/github/oliviercailloux/y2018/minimax/StrategyRandom.java
+++ src/main/java/io/github/oliviercailloux/y2018/minimax/StrategyRandom.java
@@ -24,21 +24,6 @@ import com.google.common.graph.Graph;
 import io.github.oliviercailloux.y2018.j_voting.Alternative;
 import io.github.oliviercailloux.y2018.j_voting.Voter;
 
-/**
- * Uses a random approach to get the next question: builds a set containing all
- * the questions (both to the voters and to the committee), randomly picks one.
- *
- * Problems: 1) After having asked a question, the preference knowledge changes,
- * so some question in the set might not have sense anymore. Solution: recompute
- * the set every time. Expensive. 2) Because of the rational nature of λ, there
- * exists an infinite number of questions for the committee. Solution: divide
- * the admissible interval of λ in t finite slots and update this range after
- * every answer. Note: Is there an upper bound for lambda? Should be n(m-2)
- * //TOCHECK This makes the initial admissible interval [1, n(m-2)]. Then divide
- * this, for example, in 10 slots, ask a question and depending from the answer
- * reduce the interval for the next question.
- **/
-
 public class StrategyRandom implements Strategy {
 
 	public boolean profileCompleted;
@@ -56,7 +41,7 @@ public class StrategyRandom implements Strategy {
 
 	private StrategyRandom(PrefKnowledge knowledge) {
 		final long seed = ThreadLocalRandom.current().nextLong();
-		// LOGGER.info("Using seed: {}.", seed);
+		LOGGER.info("Using seed: {}.", seed);
 		random = new Random(seed);
 		this.knowledge = knowledge;
 		profileCompleted = false;
